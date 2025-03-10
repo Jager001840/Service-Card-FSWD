623e2bff844c564f23ffc75364995d6c3d3f4348
JAGER JACKSON <jagerjackson001@gmail.com>
2025-03-10 09:35:54 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+import ServiceCard from './components/ ServiceCard.jsx'
+
+function App() {
+
+   const data = [
+    {
+      name: "Mark Johnson",
+      review: "This service was amazing! Highly recommended."
+    },
+    {
+      name: "Tailor Vivian",
+      review: "This service was okayish, would recommend if in the area."
+    },
+    {
+      name: "Vaasu Saini",
+      review: "This service was not a pleasant experience for me"
+    },
+    {
+      name: "John Dee",
+      review: "This service was the worst thing happened to me"
+    },
+
+   ]
+  return (
+    <>
+    <div id='WholeGrid'>
+      {data.map((item,index)=>(
+        <ServiceCard key= {index}{...item} />
+      ))}
+    </div>
+    </>
+  )
+}
+
+export default App
